94cc2e793f595702671d84f282431a6655e0cb0f
Werineld <92227429+Werineld@users.noreply.github.com>
2022-12-06 03:47:38 +0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,73 +53,97 @@ $(function() {
     });
 
 
+    /* copyright test */
 
-    /* Collapse */
- /*   $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
+    let block = document.querySelector('.copyright');
 
-        var $this = $(this),
-            blockId = $this.data('collapse');
+    block.addEventListener('click', ()=> {
+        if(block.classList.contains('notred')) {
+            block.classList.add('red')
+            block.classList.remove('notred')
+        }  else{
+            block.classList.remove('red')
+            block.classList.add('notred')
+        }
+    })
 
-        $this.toggleClass("active");
+    /* modals */
+
+
+    /* const service_items = document.querySelectorAll('.services_item');
+    const body = document.querySelector('body');
+
+    service_items.forEach ((service_item) => {
+     const id = service_item.dataset.path;
+     const modal = document.getElementById(id);
+     service_item.addEventListener('click', ()=> {
+        modal.classList.toggle('show');
+        body.classList.toggle('overflow');
+    });
+    
+    const close = document.querySelector('.modal_close');
+
+    close.addEventListener('click', ()=> {
+        modal.classList.toggle('show');
+        body.classList.toggle('overflow');
+    });
     });*/
 
-    $(".accordion .accordion__content:not(:first)").hide();
 
-    $(".accordion__header").click(function(){
-
-        $(this).next(".accordion__content").slideToggle("slow")
-        .siblings(".accordion__content:visible").slideUp("slow");
-        $(this).toggleClass("active");
-        $(this).siblings(".accordion__header").removeClass("active");
-     });
+    /* modal 1 ********************************/
 
 
-    /* Slider */
-    $("[data-slider]").slick({
-        focusOnSelect: true,
-        dots: false,
-        infinite: true,
-        fade: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
+
+    const btns = document.querySelectorAll('.services__item');
+
+    const modalOverlay = document.querySelector('.modal-overlay ');
+
+    const modals = document.querySelectorAll('.modal');
+
+    const body = document.querySelector('body');
+
+
+    btns.forEach((el) => {
+        el.addEventListener('click', (e) => {
+            let path = e.currentTarget.getAttribute('data-path');
+
+            modals.forEach((el) => {
+                el.classList.remove('modal--visible');
+            });
+
+            document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+            modalOverlay.classList.add('modal-overlay--visible');
+            body.classList.add('overflow')
+        });
+    });
+
+    modalOverlay.addEventListener('click', (e) => {
+        console.log(e.target);
+        const close_btn = document.querySelector('.modal_close');
+
+        if (e.target == modalOverlay) {
+            modalOverlay.classList.remove('modal-overlay--visible');
+            modals.forEach((el) => {
+                el.classList.remove('modal--visible');
+                body.classList.remove('overflow');
+            });
+        } else if (e.target == close_btn) {
+            modalOverlay.classList.remove('modal-overlay--visible');
+            modals.forEach((el) => {
+                el.classList.remove('modal--visible');
+                body.classList.remove('overflow');
+            });
+        }
+
     });
 
 
-    $('a[data-slide]').click(function(e) {
-           e.preventDefault();
-           var slideno = $(this).data('slide');
-           $('.slider-nav').slick('slickGoTo', slideno - 1);
-     });
-
-
-
-    worksSlider.slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        arrows: false,
-        dots: true
-    });
-
-    $(".slickPrev").on("click", function(event) {
-        event.preventDefault();
-
-        let currentSlider = $(this).parents('.modal').find('[data-slider="slick"]');
-
-        currentSlider.slick("slickPrev");
-    });
-
-    $(".slickNext").on("click", function(event) {
-        event.preventDefault();
-
-        let currentSlider = $(this).parents('.modal').find('[data-slider="slick"]');
-
-        currentSlider.slick("slickNext");
-    });
+ 
 
 
 
 });
+    
+    
+
 
